6417d191cd6deba895f9699892e18f21cce2a497
Akhmad Amanulloh <84214955+akhmadamanulloh@users.noreply.github.com>
2023-12-14 08:43:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -50,13 +50,11 @@ if selected == "Dataset Information":
 
 elif selected == "Klasifikasi":
   if st.session_state.nb_reduksi:
-      nb_lda, nb_NonLDA = st.tabs(["Model Naive Bayes(LDA)", "Model Naive Bayes (Tanpa LDA)"])
+      nb_lda = st.tabs("Model Naive Bayes(LDA)")
       
       with nb_lda:
         st.write(f"Prediction Category : {st.session_state.nb_reduksi}")
         
-      with nb_NonLDA:
-        st.write(f"Prediction Category : {st.session_state.nb_asli}")
         
 elif selected == "History Uji Coba":
     st.write("Hasil Uji Coba")
